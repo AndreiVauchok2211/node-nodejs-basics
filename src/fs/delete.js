430ddfbc7a087b0f05abab03a83538a259f6892f
Andrei Vauchok <vauchokandrei@gmail.com>
2024-01-23 08:50:27 +0300

    feat: implemented function delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,12 @@
-const remove = async () => {
+const fs = require('fs');
+const path = require('path');
+
+const remove =  () => {
     // Write your code here 
+    fs.unlink(path.join(__dirname, '/files/fileToRemove.txt'), err => {
+        if(err) throw new Error('Fs operation failed')
+    })
+
 };
 
-await remove();
+ remove();
