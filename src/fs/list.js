3a33d39d300a08abd25257860c42fa6c40b6b510
Andrei Vauchok <vauchokandrei@gmail.com>
2024-01-23 22:26:04 +0300

    feat: implemented function list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,13 @@
-const list = async () => {
+const fs = require('fs');
+const path = require('path')
+
+const list =  () => {
     // Write your code here 
+    fs.readdir(path.join(__dirname, '/files'), (err, files) => {
+        if(err) throw new Error('Fs operation failed');
+        console.log(files);
+    })
+
 };
 
-await list();
+ list();
