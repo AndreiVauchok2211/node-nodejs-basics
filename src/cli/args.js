7c15e829fd16dbddd1382095f19bf36df6dc4294
Andrei Vauchok <vauchokandrei@gmail.com>
2024-01-28 22:18:14 +0300

    feat: implement function parseArgs

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,9 @@
 const parseArgs = () => {
-    // Write your code here 
+  // Write your code here
+  for (let i = 2; i < process.argv.length; i++) {
+    if (process.argv[i].includes("--")) {
+      console.log(`${process.argv[i]} is ${process.argv[i + 1]}`);
+    }
+  }
 };
-
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,14 +1,10 @@
-const process = require("process");
-
 const parseEnv = () => {
   // Write your code here
-  // console.log(process.env);
   Object.keys(process.env).map((key) => {
     if (key.includes("RSS")) {
       console.log(`${key}=${process.env[key]}`);
     }
   });
-  // console.log(rss);
 };
 
 parseEnv();
