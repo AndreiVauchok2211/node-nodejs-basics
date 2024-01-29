a1d64026ec009643fc6864caa2cbb594ac4d46a5
Andrei Vauchok <vauchokandrei@gmail.com>
2024-01-30 01:00:01 +0300

    feat: add new branch

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,8 @@
-const calculateHash = async () => {
-    // Write your code here 
+const calculateHash = () => {
+  // Write your code here
+  console.log(
+    "I read late that it was necessary to make a branch from the main one and carry out development in it"
+  );
 };
 
-await calculateHash();
+calculateHash();
