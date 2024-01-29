d461f2dd10669aa8df784496424122b75746f119
Andrei Vauchok <vauchokandrei@gmail.com>
2024-01-30 00:47:44 +0300

    feat: implement block cp and module

--- src/cli/args.js
+++ src/cli/args.js
@@ -2,7 +2,7 @@ const parseArgs = () => {
   // Write your code here
   for (let i = 2; i < process.argv.length; i++) {
     if (process.argv[i].includes("--")) {
-      console.log(`${process.argv[i]} is ${process.argv[i + 1]}`);
+      console.log(`${process.argv[i].substring(2)} is ${process.argv[i + 1]}`);
     }
   }
 };
--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,15 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
+const { spawn } = require("node:child_process");
+
+const spawnChildProcess = (args) => {
+  // Write your code here
+  const childProcess = spawn("node", [__dirname + "/files/script.js", ...args]);
+  process.stdin.on("data", (data) => {
+    childProcess.stdin.write(data);
+  });
+  childProcess.stdout.on("data", (data) => {
+    process.stdout.write(data);
+  });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(["node - v", "nmp - v"]);
